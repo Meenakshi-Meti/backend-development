60caef3bdbbf96bad9c141a619fbc99317df51b6
Meenakshi <meenakshi28633@gmail.com>
2026-09-02 16:21:44 +0000

    Learn Node.js Modules and exports

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 console.log("Hello Node.js!");
 console.log("I am starting my backend development journey.");
 
 
 
+=======
+const math = require("./math");
+
+console.log("Addition :", math.add(10,5));
+console.log("Subtraction :", math.sub(10,5));
+console.log("Multiplication :",math.multiply(10,5));
+>>>>>>> 6da32ea (Learn Node.js Modules and exports)
